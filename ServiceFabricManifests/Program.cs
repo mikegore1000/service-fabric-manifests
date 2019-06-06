0652dfc7b2e9392d9679ec26b18c90499b68d20d
Mike Gore <mike.gore@asos.com>
2019-06-06 20:34:24 +0100

    Initial commit

---
+++ ServiceFabricManifests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceFabricManifests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
